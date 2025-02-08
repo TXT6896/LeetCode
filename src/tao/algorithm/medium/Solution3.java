bba7d65a9077c38de82819301e1a7607a4d7b17c
tao <1584781310@qq.com>
2025-02-09 01:37:21 +0800

    提交

--- src/tao/algorithm/medium/Solution3.java
+++ src/tao/algorithm/medium/Solution3.java
@@ -11,6 +11,13 @@ public class Solution3 {
         System.out.println(solution3.maxArea(height));
     }
 
+    /**
+     * 双指针 左右两边指针从边界往中间移动，每次移动计算当前容器大小并比较当前最大值，计算后，移动高度较低的指针
+     * 原理在于，容器大小取决于 Math.min(height[left], height[right]) * (right - left),当移动较大值时候，
+     * x轴一定减小，而高度只会更小或不变，所以移动高度较小的指针才能找到更大的容积
+     * @param height
+     * @return
+     */
     public int maxArea(int[] height) {
 
         int left = 0, right = height.length - 1, maxCapital = 0, capital;
